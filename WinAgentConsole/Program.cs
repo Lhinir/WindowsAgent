feae42b97d3f4d90c9d285047176b9cd4c3d74c9
Lhinir <99956170+Lhinir@users.noreply.github.com>
2022-02-21 17:00:06 +0300

    1.4
    5 Saat içindeki değişiklikleri gösteriyor

--- WinAgentConsole/Program.cs
+++ WinAgentConsole/Program.cs
@@ -7,19 +7,32 @@ namespace WinAgentConsole
     {
         static void Main(string[] args)
         {
+            #region Konsol Rengi
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+            #endregion
+
             EventLog log = new
             EventLog("Application");
 
+            DateTime dt = DateTime.Now.AddHours(-5.00);
+            Console.WriteLine(dt.ToString());
+
             foreach (EventLogEntry entry in log.Entries)
             {
-                Console.WriteLine("Kullanıcı Adı: " + entry.UserName);
-                Console.WriteLine("Kaynak: " + entry.Source);
-                Console.WriteLine("Girdi tipi: " + entry.EntryType);
-                Console.WriteLine("Olay id'si: " + entry.EventID);
-                Console.WriteLine("Mesaj: " + entry.Message);
-                Console.WriteLine("--------------------------------------------");
-                Console.ReadLine();
-            
+                if (entry.TimeGenerated >= dt)
+                {
+                    Console.WriteLine("Kullanıcı Adı: " + entry.UserName);
+                    Console.WriteLine("Kaynak: " + entry.Source);
+                    Console.WriteLine("Girdi tipi: " + entry.EntryType);
+                    Console.WriteLine("Olay id'si: " + entry.EventID);
+                    Console.WriteLine("Mesaj: " + entry.Message);
+                    Console.WriteLine("Oluşturulma Zamanı: " + entry.TimeGenerated);
+                    Console.WriteLine("--------------------------------------------");
+                    Console.ReadLine();
+                }
+
             }
         }
     }
